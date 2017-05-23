0230fa27e06a5a1c75074630d701338a3da98f04
Mario Affenzeller <mario-affenzeller@gmx.at>
2017-05-23 17:25:51 +0200

    Login (stillgelegt) & Home (anfang)

---
+++ Web APP/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_APP.Controllers
+{
+    public class LoginController : Controller
+    {
+        //1.Schritt Login
+        public ActionResult Login()
+        {
+            //LoginModel test = new LoginModel();
+            //ViewBag.IDKunde = new SelectList(dataseven.Login, "id", "name", "password");
+
+            return View(
+                //test
+                );
+        }
+        //[HttpPost]
+        //public ActionResult Login(id, name, password)
+        //{
+        //    return View();
+        //}
+
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
