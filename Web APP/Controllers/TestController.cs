4775717605cf86a22711d77c963e4f61a96e23ed
Mario Affenzeller <mario-affenzeller@gmx.at>
2017-05-30 14:53:43 +0200

    Datenbank-Mapping
    EntityFramework6.Npgsql  installiert und erste Schritte für die übergabe der Daten erfolgt

---
+++ Web APP/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Web_APP.Models.Model1.edmx.Model1.tt;
+using Web_APP.Models;
+
+namespace Web_APP.Controllers
+{
+    public class TestController : Controller
+    {
+        //private Model1Entities db = new Model1Entities();
+
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            TestModel TM = new TestModel();
+
+            Model1Entities db = new Model1Entities();
+
+            List<Model1Entities> meList = new List<Model1Entities>();
+
+            datasevenEntities1 dse = new datasevenEntities1();
+
+            dse.stamm.Where(x => x.id == 100001).ToList();
+
+       
+
+
+
+
+            return View(dse.stamm.Where(x => x.id == 100001).ToList());
+        }
+    }
+}
---
+++ Web APP/Models/TestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_APP.Models
+{
+    public class TestModel
+    {
+        public int id
+        {
+            get; set;
+        }
+        public string vorname
+        {
+            get; set;
+        }
+        public string name
+        {
+            get; set;
+        }
+
+    }
+}
